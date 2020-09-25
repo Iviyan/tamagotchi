d9f65f458a78db0c2634e23de7405916f3afcca3
Zagadochnik37 <Elephant-5@yandex.ru>
2020-09-25 23:08:13 +0300

    +avatar

---
+++ tamagotchi/ConsoleDraw.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamagotchi
+{
+    public static class ConsoleDraw
+    {
+        static ConsoleColor ClosestConsoleColor(Color cl)
+        {
+            ConsoleColor ret = 0;
+            double rr = cl.R, gg = cl.G, bb = cl.B, delta = double.MaxValue;
+
+            foreach (ConsoleColor cc in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                var n = Enum.GetName(typeof(ConsoleColor), cc);
+                var c = System.Drawing.Color.FromName(n == "DarkYellow" ? "Orange" : n); // bug fix
+                var t = Math.Pow(c.R - rr, 2.0) + Math.Pow(c.G - gg, 2.0) + Math.Pow(c.B - bb, 2.0);
+                if (t == 0.0)
+                    return cc;
+                if (t < delta)
+                {
+                    delta = t;
+                    ret = cc;
+                }
+            }
+            return ret;
+        }
+
+        public static void ConsoleWriteImage(Bitmap src, int start_y = -1, int start_x = 0)
+        {
+            if (start_y >= 0) Console.CursorTop = start_y;
+            for (int i = 0; i < src.Height; i++)
+            {
+                Console.CursorLeft = start_x;
+                for (int j = 0; j < src.Width; j++)
+                {
+                    ConsoleColor c = ClosestConsoleColor(src.GetPixel(j, i));
+                    Console.ForegroundColor = c;
+                    Console.Write("██");
+                }
+                if (!(Console.CursorLeft < start_x + src.Width)) Console.CursorTop++;
+                
+            }
+        }
+    }
+}
--- tamagotchi/Program.cs
+++ tamagotchi/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using tamagotchi.Properties;
 
 namespace tamagotchi
 {
@@ -11,17 +13,38 @@ namespace tamagotchi
     {
         public static int health = 100;
 
-        public void draw()
-        {
+        static int Wheight = 120;
+        static int Wwidth = 30;
 
+        public static void drawAvatar()
+        {
+            Console.Title = health.ToString();
+            switch (health)
+            {
+                case 99: ConsoleDraw.ConsoleWriteImage(new Bitmap(Resources.stickman1), 0, Console.WindowWidth - 31); break;
+                case 75: ConsoleDraw.ConsoleWriteImage(new Bitmap(Resources.stickman2), 0, Console.WindowWidth - 31); break;
+                case 50: ConsoleDraw.ConsoleWriteImage(new Bitmap(Resources.stickman3), 0, Console.WindowWidth - 31); break;
+                case 25: ConsoleDraw.ConsoleWriteImage(new Bitmap(Resources.stickman4), 0, Console.WindowWidth - 31); break;
+            }
         }
         public static void loop()
         {
             for (;;)
             {
                 //Console.WriteLine(Console.WindowWidth);
-                Thread.Sleep(1000);
+                Thread.Sleep(100);
                 health--;
+                
+                if (Console.WindowWidth != Wwidth || Console.WindowHeight != Console.WindowHeight)
+                {
+                    Console.Clear();
+                }
+
+                drawAvatar();
+
+                Wheight = Console.WindowHeight;
+                Wwidth = Console.WindowWidth;
+                //Console.Title = $"{Wwidth} x {Wheight}";
             }
         }
         static void Main(string[] args)
@@ -29,6 +52,10 @@ namespace tamagotchi
             
             Thread back = new Thread(new ThreadStart(loop));
             back.Start();
+            /*for (int i = 0; i < 120; i++) Console.Write("█");
+            Console.CursorTop++;
+            Console.Write("█");
+            Console.Read();*/
         }
 
 
