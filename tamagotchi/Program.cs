08f8c6d4fe4400d1217fd16f6a6cfc546423552c
Zagadochnik37 <Elephant-5@yandex.ru>
2020-09-25 19:09:29 +0300

    Добавьте файлы проекта.

---
+++ tamagotchi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tamagotchi
+{
+    class Program
+    {
+        public static int health = 100;
+
+        public void draw()
+        {
+
+        }
+        public static void loop()
+        {
+            for (;;)
+            {
+                //Console.WriteLine(Console.WindowWidth);
+                Thread.Sleep(1000);
+                health--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Thread back = new Thread(new ThreadStart(loop));
+            back.Start();
+        }
+
+
+    }
+}
+
+/*Справа вверху - аватар
+ * руки опускаются со здоровьем
+ * под аватаром - 95/100
+ * меню - деньги, действия
+ * работа - миниигра, нажимать на клавиши, высвечивающиеся на экране
+ * покормить / вылечить
+ * болезнь
+ * 
+ * 
+ */
